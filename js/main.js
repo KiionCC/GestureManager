59a7d17e14d0c94f422d27958660b5adad02e4aa
GetALife <1340482017@qq.com>
2019-06-20 13:57:30 +0800

    :construction: enable restart

--- js/main.js
+++ js/main.js
@@ -15,24 +15,25 @@ new Vue({
     methods: {
         start: function () {
             if (pid != -1) {
+
                 console.log('killing python...', pid)
                 pypc.kill('SIGINT')
 
             }
-            else {
-                //运行py
-
-                var workerProcess = child_process.spawn('python', ['real_time.py', '4242'])
-                if (workerProcess != null) {
-                    console.log('python process start successful', workerProcess.pid)
-                }
-                workerProcess.on('close', (code, signal) => {
-                    console.log(`子进程收到信号 ${signal} 而终止`);
-                    pid = -1
-                });
-                pid = workerProcess.pid
-                pypc = workerProcess
+            //首次开启
+            //运行py
+            var workerProcess = child_process.spawn('python', ['real_time.py', '4242'])
+            if (workerProcess != null) {
+                //启动成功
+                console.log('python process start successful', workerProcess.pid)
             }
+            workerProcess.on('close', (code, signal) => {
+                //关闭成功
+                console.log(`子进程收到信号 ${signal} 而终止`);
+                pid = -1
+            });
+            pid = workerProcess.pid
+            pypc = workerProcess
         }
     }
 })
